4fed531659a297fec78075310578a8bc51ff23da
ogatamegumi <mmmegumi.33@gmail.com>
2025-09-22 18:57:14 +0900

    詳細コンポーネントへ遷移するリンク追加

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,12 +5,19 @@ import { createApp } from 'vue';
 import HeaderComponent from './components/HeaderComponent.vue';
 import TaskListComponent from './components/TaskListComponent.vue';
 import ExampleComponent from './components/ExampleComponent.vue';
+import TaskShowComponent from './components/TaskShowComponent.vue';
 
 const routes = [
   {
     path: '/tasks',
     name: 'task.list',
     component: TaskListComponent
+  },
+  {
+    path: '/tasks/:taskId',
+    name: 'task.show',
+    component: TaskShowComponent,
+    props: true
   },
 ];
 
