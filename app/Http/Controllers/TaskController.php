fe953eec46b51e5de41139a9a552ee720cc92578
ogatamegumi <mmmegumi.33@gmail.com>
2025-09-23 13:57:14 +0900

    for destroy

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -22,9 +22,16 @@ class TaskController extends Controller
         return Task::create($request->all());
     }
 
-    public function update(Request $request, Task $Task)
+    public function update(Request $request, Task $task)
     {
         $task->update($request->all());
+
+        return $task;
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
 
         return $task;
     }
