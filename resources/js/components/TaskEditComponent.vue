1a11ecac11bbbd015eb450d491aa7944a50da9fa
ogata megumi <mmmegumi.33@gmail.com>
2025-09-24 14:19:59 +0900

    add '>' to template

--- resources/js/components/TaskEditComponent.vue
+++ resources/js/components/TaskEditComponent.vue
@@ -9,7 +9,7 @@
           </div>
           <div class="form-gropu row">
             <label for="title" class="col-sm-3 col-form-label">Title</label>
-            <input type="text" class="col-sm-9 form-control" id="title" v-model="task.title"
+            <input type="text" class="col-sm-9 form-control" id="title" v-model="task.title">
           </div>
           <div class="form-gropu row">
             <label for="content" class="col-sm-3 col-form-label">Content</label>
