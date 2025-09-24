515a8b94b91193138ed4aa833823b63430f23b38
ogata megumi <mmmegumi.33@gmail.com>
2025-09-24 16:43:36 +0900

    factory作成/タイトルのtest追加（エラー時）

---
+++ tests/Feature/TaskFormTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Http\Requests\TaskForm;
+use App\Models\Task;
+
+class TaskFormTest extends TestCase
+{
+    public function test_error_with_empty_title(): void
+    {
+        $task = Task::factory()->create();
+
+        $response = $this->putJson("/api/tasks/{$task->id}", [
+            'title' => '',
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['title']);
+        $response->assertJsonFragment([
+            'title' => ['タイトルは必須です。']
+        ]);
+    }
+
+    public function test_error_with_over_25_character_title(): void
+    {
+        $task = Task::factory()->create();
+        $longTitle = str_repeat('あ', 256);
+
+        $response = $this->putJson("/api/tasks/{$task->id}", [
+            'title' => $longTitle,
+        ]);
+
+        $response->assertJsonValidationErrors(['title']);
+        $response->assertJsonFragment([
+            'title' => ['タイトルは255字以内で入力してください。']
+        ]);
+    }
+}
