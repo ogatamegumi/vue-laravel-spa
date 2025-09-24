f8f024ac82d35355567646484f4906e2db167413
ogatamegumi <mmmegumi.33@gmail.com>
2025-09-24 11:41:22 +0900

    Task.phpを記述

---
+++ tests/Unit/TaskModelTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use App\Models\Task;
+
+class TaskModelTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
